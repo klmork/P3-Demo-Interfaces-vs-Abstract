c5d04a043d1a3a42f9bbc668779f81314cbb840f
klmork <klmork@calpoly.edu>
2023-05-10 09:36:24 -0700

    Update Pet.java
    add modifier

--- src/Pet.java
+++ src/Pet.java
@@ -2,7 +2,7 @@ public abstract class Pet {
     private int random;
 
     public Pet(int r) { this.random = r; }
-    abstract void speak();
+    public abstract void speak();
     public void randomMethod() {
         System.out.println(random + " cool cool cool");
 
