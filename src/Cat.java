e985f3ab4630bd65fa7919c2941163c72e344139
klmork <klmork@calpoly.edu>
2023-10-13 09:03:32 -0700

    Interfaces: Pre Java 8 Example

--- src/Cat.java
+++ src/Cat.java
@@ -1,8 +1,8 @@
-public class Cat {
-
+public class Cat implements Pet{
     public void speak() {
         System.out.println("meow");
     }
-
-
+    public int  eat(int amount) {
+        return amount + 1;
+    }
 }
--- src/Dog.java
+++ src/Dog.java
@@ -1,8 +1,12 @@
-public class Dog {
-
+public class Dog implements Pet {
     public void speak() {
         System.out.println("bark");
     }
-
+    public int eat(int amount) {
+        return amount + 2;
+    }
+    public void sit() {
+        System.out.println("Good dog");
+    }
 
 }
