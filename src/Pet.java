c7b6223b2c7511b0bc1b601654cad595f7a25989
klmork <klmork@calpoly.edu>
2023-10-18 09:47:22 -0700

    Part 3: Default Methods and Multiple Interfaces
    This lecture we saw:
    - using default methods to remove duplication (with eat method)
       - we had to declare abstract getter / setter methods to handle
         the instance variables
    - Making classes implement multiple interfaces (Dog implements
      Robot and Pet
    - Making interfaces extend other interfaces (Pet extends Robot),
      but we later changed this back.

--- src/Pet.java
+++ src/Pet.java
@@ -1,12 +1,17 @@
 public interface Pet {
     void speak();
-    boolean eat(int amount);
     // TODO: add default method to print something generic
     default void sayPet() {
         System.out.println("I am a " + this.getClass());
     }
 
-
+    default boolean eat(int amount) {
+        setAmount(amount);
+        return getAmountFed() > getFullAt();
+    }
+    void setAmount(int a);
+    int getAmountFed();
+    int getFullAt();
 
     // TODO: add default methods to remove duplication
 
